b9785696a85ae2bd014dffff6e47fc77d9bf19f8
Buzunda <luizrobertoavila@gmail.com>
2022-03-19 19:22:39 -0300

    Add InputBoxes component with initial configurations

---
+++ src/Shared/Components/InputBoxes/InputBoxes.jsx
@@ -0,0 +1,95 @@
+import React, { useEffect, useState } from "react";
+import PropTypes from "prop-types";
+
+import InputBox from "../InputBox/InputBox";
+
+const InputBoxes = ({ amount, handleOutputString }) => {
+  const [word, setWord] = useState(Array(amount).fill(null));
+
+  const inputRegExp = /^[a-zA-Z]$/;
+
+  const inputElements = [];
+
+  useEffect(() => {
+    inputElements["input-0"].focus();
+  }, []);
+
+  useEffect(() => {
+    handleOutputString(word.join(""));
+  }, [word]);
+
+  const focusPreviousInput = (target) => {
+    if (target.previousElementSibling !== null) {
+      target.previousElementSibling.focus();
+    }
+  };
+
+  const focusNextInput = (target) => {
+    if (target.nextElementSibling !== null) {
+      target.nextElementSibling.focus();
+    }
+  };
+
+  const setValue = () => {
+    let updatedCharacters = word.map((character, number) => {
+      return inputElements["input-" + number].value;
+    });
+
+    setWord(updatedCharacters);
+  };
+
+  const handleChange = ({ target }) => {
+    if (target.value.match(inputRegExp)) {
+      focusNextInput(target);
+      setValue(target);
+    } else {
+      target.value = word[target.name.replace("input-", "")];
+    }
+  };
+
+  const handleKeyDown = ({ target, key }) => {
+    if (key === "Backspace") {
+      if (target.value === "" && target.previousElementSibling !== null) {
+        target.previousElementSibling.value = "";
+        focusPreviousInput(target);
+      } else {
+        target.value = "";
+      }
+      setValue(target);
+    } else if (key === "ArrowLeft") {
+      focusPreviousInput(target);
+    } else if (key === "ArrowRight" || key === " ") {
+      focusNextInput(target);
+    }
+  };
+
+  const renderItems = () => {
+    let items = [];
+
+    for (let i = 0; i < amount; i++) {
+      items.push(
+        <InputBox
+          key={i}
+          handleKeyDown={handleKeyDown}
+          handleChange={handleChange}
+          inputRef={(el) => {
+            if (!el) return;
+            inputElements[el.name] = el;
+          }}
+          name={"input-" + i}
+        />
+      );
+    }
+
+    return items;
+  };
+
+  return <div>{renderItems()}</div>;
+};
+
+InputBoxes.propTypes = {
+  amount: PropTypes.number,
+  handleOutputString: PropTypes.func.isRequired,
+};
+
+export default InputBoxes;
---
+++ src/Shared/Components/InputBoxes/InputBoxes.test.jsx
@@ -0,0 +1,131 @@
+import React from "react";
+import { render, screen, fireEvent } from "@testing-library/react";
+import getGiven from "givens";
+
+import InputBoxes from "./InputBoxes";
+
+describe("InputBoxes", () => {
+  const given = getGiven();
+
+  given("amount", () => 5);
+  const handleOutputString = jest.fn();
+
+  const fillInput = (testId, value) => {
+    fireEvent.change(screen.getByTestId(testId), {
+      target: { value: value },
+    });
+  };
+
+  const fillActiveInput = (value) => {
+    fireEvent.change(document.activeElement, {
+      target: { value: value },
+    });
+  };
+
+  const fillWord = (word) => {
+    for (let i = 0; i < word.length; i++) {
+      fillActiveInput(word[i]);
+    }
+  };
+
+  const keydownActiveInput = (key) => {
+    fireEvent.keyDown(document.activeElement, {
+      key: key,
+      code: key,
+    });
+  };
+
+  beforeEach(() => {
+    render(
+      <InputBoxes
+        handleOutputString={handleOutputString}
+        amount={given.amount}
+      />
+    );
+  });
+
+  it("generates the amount of input boxes passed on prop", () => {
+    expect(screen.getAllByRole("textbox")).toHaveLength(given.amount);
+  });
+
+  describe("navigation", () => {
+    it("first element is focused", () => {
+      expect(
+        document.activeElement === screen.getByTestId("input-0")
+      ).toBeTruthy();
+    });
+    it("right arrow key moves the focus right", () => {
+      keydownActiveInput("ArrowRight");
+      expect(
+        document.activeElement === screen.getByTestId("input-1")
+      ).toBeTruthy();
+    });
+
+    it("left arrow key moves the focus left", () => {
+      screen.getByTestId("input-1").focus();
+
+      keydownActiveInput("ArrowLeft");
+      expect(
+        document.activeElement === screen.getByTestId("input-0")
+      ).toBeTruthy();
+    });
+
+    describe("input", () => {
+      it("filling a value moves the focus right", () => {
+        fillActiveInput("a");
+        expect(
+          document.activeElement === screen.getByTestId("input-1")
+        ).toBeTruthy();
+      });
+
+      it("filling a value keeps the focus if element is the last", () => {
+        screen.getByTestId(`input-${given.amount - 1}`).focus();
+        fillActiveInput("a");
+        expect(
+          document.activeElement ===
+            screen.getByTestId(`input-${given.amount - 1}`)
+        ).toBeTruthy();
+      });
+    });
+
+    describe("backspace", () => {
+      it("moves the focus left if current input is empty", () => {
+        screen.getByTestId("input-1").focus();
+        keydownActiveInput("Backspace");
+        expect(
+          document.activeElement === screen.getByTestId("input-0")
+        ).toBeTruthy();
+      });
+
+      it("keeps the focus on input if current input has value", () => {
+        fillInput("input-1", "a");
+        screen.getByTestId("input-1").focus();
+        keydownActiveInput("Backspace");
+        expect(
+          document.activeElement === screen.getByTestId("input-1")
+        ).toBeTruthy();
+      });
+    });
+  });
+
+  describe("filling the inputs", () => {
+    it("calls handleOutputString with the word informed", () => {
+      const word = "among";
+      fillWord(word);
+      expect(handleOutputString).toHaveBeenCalledWith("among");
+    });
+
+    it("calls handleOutputString with the word informed after updating it", () => {
+      const word = "grade";
+
+      fillWord(word);
+
+      keydownActiveInput("Backspace");
+      keydownActiveInput("Backspace");
+
+      fillWord("ss");
+
+      expect(handleOutputString).toHaveBeenCalledWith("grass");
+    });
+  });
+});
