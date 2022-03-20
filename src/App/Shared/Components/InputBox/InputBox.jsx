e43616ee16ec87cb9e682ffefe72e4b32c2f2466
Buzunda <luizrobertoavila@gmail.com>
2022-03-20 10:57:46 -0300

    Add status prop to inputBox component

--- src/App/Shared/Components/InputBox/InputBox.jsx
+++ src/App/Shared/Components/InputBox/InputBox.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import PropTypes from "prop-types";
+import classNames from "classnames/bind";
 
 import styles from "./InputBox.module.scss";
 
+const cx = classNames.bind(styles);
+
 const InputBox = ({
   name,
+  status,
   inputRef,
   inputProps,
   handleKeyDown,
@@ -13,7 +17,7 @@ const InputBox = ({
   return (
     <input
       {...inputProps}
-      className={styles.inputBox}
+      className={cx("inputBox", status)}
       type="text"
       onKeyDown={handleKeyDown}
       onChange={handleChange}
@@ -27,6 +31,7 @@ const InputBox = ({
 
 InputBox.propTypes = {
   name: PropTypes.string.isRequired,
+  status: PropTypes.string,
   inputRef: PropTypes.func,
   inputProps: PropTypes.object,
   handleKeyDown: PropTypes.func,
--- src/App/Shared/Components/InputBox/InputBox.test.jsx
+++ src/App/Shared/Components/InputBox/InputBox.test.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { render, screen, fireEvent } from "@testing-library/react";
+import getGiven from "givens";
 
 import InputBox from "./InputBox";
 
 describe("InputBox", () => {
+  const given = getGiven();
   const handleKeyDown = jest.fn();
   const handleChange = jest.fn();
 
@@ -11,12 +13,14 @@ describe("InputBox", () => {
     render(
       <InputBox
         type={"text"}
+        status={given.status}
         handleKeyDown={handleKeyDown}
         handleChange={handleChange}
         name={"input-test"}
       />
     );
   });
+
   describe("on keydown", () => {
     it("calls handleKeyDown callback", () => {
       fireEvent.keyDown(screen.getByTestId("input-test"), {
@@ -33,6 +37,26 @@ describe("InputBox", () => {
         target: { value: "A" },
       });
       expect(handleChange).toHaveBeenCalled();
+    });
+  });
+
+  ["correct", "wrong", "misplaced"].forEach((status) => {
+    describe(`for status ${status}`, () => {
+      given("status", () => status);
+
+      it(`adds ${status} class`, () => {
+        expect(screen.getByTestId("input-test")).toHaveClass(status);
+      });
+    });
+  });
+
+  describe("when there is no status", () => {
+    given("status", () => null);
+
+    it("does not add any extra class", () => {
+      const element = screen.getByTestId("input-test");
+      expect(element).toHaveClass("inputBox");
+      expect(element.classList).toHaveLength(1);
     });
   });
 });
