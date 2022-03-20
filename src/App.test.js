c7d61c7fc952a6f0edd989b494fb7cdc9ea6d605
Buzunda <luizrobertoavila@gmail.com>
2022-03-20 08:57:16 -0300

    Update App component to use Main component

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,16 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+import { fillWord } from "./Test/Helpers";
+import React from "react";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("App", () => {
+  beforeEach(() => {
+    render(<App />);
+  });
+
+  it("renders the filled word", () => {
+    fillWord("super");
+
+    expect(screen.queryByText("super")).toBeInTheDocument();
+  });
 });
