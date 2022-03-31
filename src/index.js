a1f348f84f3f8d9f57551977fa3f835bdcf66a24
Buzunda <luizrobertoavila@gmail.com>
2022-03-31 18:22:28 -0300

    Add Redux store and KeyWatcher to the App

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
 
 import App from "./App/App";
 import reportWebVitals from "./reportWebVitals";
 import "./index.scss";
+import store from "./App/Store/store";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
